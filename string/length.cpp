ca453669a66f34143b87cf45ebb525639679f27d
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-08-31 14:40:45 +0530

    string 1

---
+++ string/length.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+     
+     char *s = "hello";
+     int i;
+     for ( i = 0; s[i] != 0; i++)
+     {
+         
+     }
+     cout<<i;
+
+    return 0;
+}
