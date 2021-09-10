f5a1d9836fe5c1e5377c33a2bf52855be06c95d8
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-10 23:28:56 +0530

    binary  search

---
+++ array/binary search.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+  int n,arr[n],i,key,l,h,mid;
+  mid=(l+h)/2;
+  cout<<"enter the length of the array: ";
+  cin>>n;
+  cout<<"enter the elements of the array: ";
+  for ( i = 0; i < n; i++)
+  {
+    cin>>arr[i];
+  }
+  cout<<"enter the key element: ";
+  cin>>key;
+  
+ l=arr[0];
+ h=arr[n];
+ while (l<=h)
+ {
+    if (mid==key)
+    {
+       cout<<"key is found and is "<<mid;
+    }
+    else if (mid>key)
+    {
+       l=mid+1;
+    }
+    else{
+       h=mid-1;
+    }
+    
+ }
+ 
+   return 0;
+}
