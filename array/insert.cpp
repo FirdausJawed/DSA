a894b467c323347cb2bead6f3356a75c749cc371
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-10 13:17:13 +0530

    insert

--- array/insert.cpp
+++ array/insert.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 int main (){
-
-
+  int n,i,new_array,arr[n];
+  cout<<"enter the length of the array";
+  cin>>n;
+  cout<<"enter the elements of the array!!";
+  for ( i = 0; i < n; i++)
+  {
+     cin>>arr[i];
+  }
+  cout<<"enter the element that  have to be added to the array";
+  cin>>new_array;
+  arr[i] = new_array;
+  for ( i = 0; i < n+1; i++)
+  {
+     cout<<arr[i]<<' ';
+  }
+  cout<<endl;
     return 0;
 }
