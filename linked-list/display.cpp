4065e179ca50fdf355b249a928a31dca34caf519
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-16 13:47:26 +0530

    display linked list

--- linked-list/display.cpp
+++ linked-list/display.cpp
@@ -2,14 +2,49 @@
 using namespace std;
 
 struct node{
-    int dsts;
-    struct Node *next;
+    int data;
+    struct node *next;
 }*first = NULL;
 
 
 void create (int A[], int n){
     int i;
-    struct Node *t, *last;
-    first = new struct Node;
+    struct node *t, *last;
+    first = new struct node;
     first -> data = A[0];
+    first -> next = NULL;
+    last = first;
+    for ( i = 0; i < n; i++)
+    {
+        t = new struct node;
+        t -> data = A[i];
+        t ->next = NULL;
+        last -> next = t;
+        last =t;
+    }
+    
+}
+
+void display (struct node *p){
+    while (p != NULL)
+    {
+       cout<< p -> data;
+       p = p -> next;
+    }
+    
+}
+
+void Rdisplay (struct node *p){
+    if (p != NULL){
+        Rdisplay (p -> next);
+        cout<< p -> data; 
+    }
+}
+
+int main (){
+    struct node *temp;
+    int A[] = {2,4,3,8,5,9,10};
+    create (A,8);
+    display (first);
+    return 0;
 }
