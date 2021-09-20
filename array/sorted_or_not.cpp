5ba7cd196defecffffc3f4c78663719486080c2a
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-20 13:02:28 +0530

    sorted or not

--- array/sorted_or_not.cpp
+++ array/sorted_or_not.cpp
@@ -9,18 +9,22 @@ int main (){
        cin>>arr[j];
     
     }
+    int marker = 1;
     for (int  i = 0; i < n-1; i++)
     {
-       if (arr[i]>arr[i+1])
-       {
-           cout<<"not sorted";    
-       }
-       else
-       {
-       cout<<"sorted";
-       break;
-       }
+        if (arr[i]>arr[i+1])
+        {
+           marker = 0;
+           break;
+        }
+        
     }
-    
+    if (marker==0)
+    {
+       cout<<"not sorted";
+    }
+    else{
+        cout<<"sorted";
+    }
     return 0;
 }
