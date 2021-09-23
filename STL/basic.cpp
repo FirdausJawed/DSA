04161a897441fd75c02337b5dfa08951713906b0
FidrausJawed01 <firdausj.ug20.ee@nitp.ac.in>
2021-09-23 15:06:30 +0530

    first templete

---
+++ STL/basic.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+template <typename T>
+int search (T A[],int n,T key){
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i]==key)
+        {
+            return i;
+        }
+        
+    }
+    return -1;
+}
+
+int main (){
+
+   int n;
+   cin>>n;
+   int A[n];
+
+   for (int i = 0; i < n; i++)
+   {
+       cin>>A[i];
+   }
+   
+   int key;
+   cin>>key;
+   
+   cout<<search (A,n,key)<<endl;
+
+    return 0;
+}
