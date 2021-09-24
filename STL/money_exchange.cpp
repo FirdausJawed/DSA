5db5ae58c0e09c9e406518a8ccee20715388698d
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-25 00:48:17 +0530

    money exchange problem

---
+++ STL/money_exchange.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool compare (int a, int b){
+    return a<=b;
+}
+
+int main(){
+
+   int coins[] = {1,2,5,10,20,50,100,200,500,2000};
+   int n = sizeof(coins)/sizeof(int);
+   int money ;
+   cin>>money;
+
+   while(money>0){
+       auto lb = lower_bound(coins,coins+n,money,compare)-coins-1;
+       int m = coins[lb];
+       cout<<m<<" ";
+       money = money-m;
+   }
+
+    return 0;
+}
