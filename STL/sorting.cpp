0130522008aa1edf00ec05819267faf270d10d1f
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-25 00:19:49 +0530

    sorting algorithm

---
+++ STL/sorting.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[1000];
+
+    for (int i = 0; i < n; i++)
+    {
+      cin>>arr[i];
+    }
+    
+    //sorting
+    sort(arr,arr+n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<",";
+    }
+    
+
+    return 0;
+}
