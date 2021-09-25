1e5b187dd15c54451a28569a25a420a025f1915d
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-25 11:32:17 +0530

    pair

---
+++ STL/pair.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main (){
+
+   pair<int,char> p;
+   p.first=10;
+   p.second='F';
+   cout<<p.first<<"  "<<p.second<<endl;
+
+   //another pair using first one
+    pair<int,char> p2(p);
+    cout<<p2.first<<"  "<<p2.second<<endl;
+
+   //pair with string
+    pair<int,string>p3 = make_pair(100,"Firdaus");
+    cout<<p3.first<<"  "<<p3.second<<endl;
+
+    //take input;
+    int a,b;
+    cin>>a>>b;
+    pair<int,int>p4 = make_pair(a,b);
+    cout<<p4.first<<"  "<<p4.second<<endl;
+       
+    return 0;
+}
