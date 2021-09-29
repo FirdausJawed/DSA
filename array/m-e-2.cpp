8cb6ef65c60e8b46c3dd53d276fba26cf728bf60
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-29 12:00:01 +0530

    2

--- array/m-e-2.cpp
+++ array/m-e-2.cpp
@@ -6,5 +6,17 @@ int main (){
     int arr[]={1,2,3,4,6,7,8,9};
     int n = sizeof(arr)/sizeof(int);
 
+    int diff = arr[0]-0;
+    for (int i = 0; i < n; i++)
+    {
+       if (arr[i]-i != diff)
+       {
+           cout<<"missing element is "<<i+diff<<endl;
+           break;
+       }
+       
+    }
+    
+
     return 0;
 }
