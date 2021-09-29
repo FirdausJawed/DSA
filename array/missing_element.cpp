8d5cc5a58a94d164f4c574c64321a058f33a5332
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-29 11:33:03 +0530

    1

---
+++ array/missing_element.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+   int arr[]={1,2,3,4,6,7,8,9};
+   int n = sizeof(arr)/sizeof(int);
+   int sum=0;
+   for (int i = 0; i < n; i++)
+   {
+     sum=sum+arr[i];
+   }
+
+   int s = (n*(n+1))/2;
+   
+   int x=s-sum;
+
+   cout<<x<<endl;
+
+    return 0;
+}
+
