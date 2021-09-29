de95b9490397f2ec3383bd3c493da78f0eb9600a
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-29 12:39:59 +0530

    4

--- array/m-e-3.cpp
+++ array/m-e-3.cpp
@@ -3,8 +3,22 @@ using namespace std;
 
 int main (){
 
-    int arr[]={1,2,3,4,6,7,8,9};
+    int arr[]={1,2,3,4,6,7,8,9,10,13,14,15};
     int n = sizeof(arr)/sizeof(int);
+    
+    int diff=arr[0]-0;
+    for (int  i = 0; i < n; i++)
+    {
+       if (diff<arr[i]-i)
+       {
+          cout<<diff+i<<endl;
+          diff++;
+       }
+       
+    }
+    
+
+
 
     return 0;
 }
