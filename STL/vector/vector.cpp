8db18b166eee5a1508960724f19bef762b162375
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-09-30 12:15:51 +0530

    .

--- STL/vector/vector.cpp
+++ STL/vector/vector.cpp
@@ -3,9 +3,22 @@
 using namespace std;
 int main (){
 
-   vector<int>v{1,2,3,4,5};
+   vector<int>d{1,2,3,4,5};
+   d.push_back(16);
+   //d.pop_back();
+   d.insert(d.begin()+3,4,1000);
+   d.erase(d.begin());
+   cout<<d.size()<<endl;
 
+   cout<<d.capacity()<<endl;
+   //d.clear();
 
-   
+   cout<<d.front()<<endl;
+   cout<<d.back()<<endl;
+
+   for(int x: d)
+   {
+    cout<<x<<" _ ";
+   }
     return 0;
 }
