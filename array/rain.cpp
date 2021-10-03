dd035c19a8fba832c188fb3ec25157ad0d24fac8
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-03 19:06:23 +0530

    2

--- array/rain.cpp
+++ array/rain.cpp
@@ -10,7 +10,25 @@ int main (){
        cin>>arr[i];
     }
     
+    int res=0;
+    for (int i = 0; i < n-1; i++)
+    {
+        int imax=arr[i];
 
+        for (int j = 0; i < i; j++)
+        {
+           imax=max(imax,arr[j]);
+        }
 
+        int rmax=arr[i];
+        for (int j = i+1; j < n; j++)
+        {
+            rmax=max(rmax,arr[j]);
+        }
+        res=res+min(imax,rmax)-arr[i];
+    }
+
+    cout<<res<<endl;
+    
     return 0;
 }
