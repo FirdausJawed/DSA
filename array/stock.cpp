b354fa86325a4ac93bec42bc10b5acb43f1ca05b
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-03 19:31:53 +0530

    3

---
+++ array/stock.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+   
+    int n;
+    cin>>n;
+    int price [n];
+    for (int i = 0; i < n; i++)
+    {
+       cin>>price[i];
+    }
+    
+    int profit=0;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (price[i+1]>price[i])
+        {
+           profit+=price[i+1]-price[i];
+        }
+        
+    }
+    cout<<profit<<" "<<endl;
+
+    return 0;
+}
