b8c54968fcbd14c8c91498b53f6a1d693a84a32c
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-19 17:55:44 +0530

    added list

--- STL/list/1.cpp
+++ STL/list/1.cpp
@@ -22,6 +22,49 @@ int main (){
    for(string s:l2){
        cout<<s<<"-->";
    } 
-   
+     cout<<endl;
+
+   // some more functions to perform
+
+   l2.push_back("orange");
+   l2.push_back("lemon");
+
+    for(string s:l2){
+       cout<<s<<" ";
+   } 
+    cout<<endl;
+
+    //remove fruit
+
+    string f;
+    cin>>f;
+    l2.remove("guava");
+    for(string s:l2){
+       cout<<s<<"-->";
+   } 
+  cout<<endl;
+
+   //arase one or more elements
+
+   auto it = l2.begin();
+   it++;
+   it++;
+   l2.erase(it);
+
+ for(string s:l2){
+       cout<<s<<"-->";
+   } 
+  cout<<endl;
+   //we caqn insert an element
+
+   it=l2.begin();
+   it++;
+
+   l2.insert(it,"fruit juice");
+    for(string s:l2){
+       cout<<s<<"-->";
+   } 
+  cout<<endl;
+
     return 0;
 }
