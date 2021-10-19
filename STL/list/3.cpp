9c6bfc88fa949973bef698ccafed42dd9afecb60
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-19 18:34:23 +0530

    added forward list

---
+++ STL/list/3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<list>
+using namespace std;
+int main (){
+
+    //list creating graph data structure
+
+    list<pair<int,int>> *l;
+    int n;
+    cin>>n;
+    l=new list <pair<int,int>> [n];
+
+    int e;
+    cin>>e;
+
+    for (int i = 0; i < e; i++)
+    {
+        int x,y,wt;
+        cin>>x>>y>>wt;
+
+        l[x].push_back(make_pair(y,wt));
+        l[y].push_back(make_pair(x,wt));
+
+    }
+    
+   for (int i = 0; i < n; i++)
+   {
+       for(auto xp: l[i]){
+           cout<<"("<<xp.first<<","<<xp.second<<"), ";
+       }
+
+       cout<<endl;
+   }
+
+    return 0;
+}
