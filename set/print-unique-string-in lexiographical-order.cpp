13a10240479ec3e9d9723942a5dde6eac1b05a44
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-25 22:46:12 +0530

    added a problem of set

---
+++ set/print-unique-string-in lexiographical-order.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main (){
+  
+  set<string>s;
+  int n;
+  cin>>n;
+  for (int i = 0; i < n; i++)
+  {
+    string str;
+    cin>>str;
+    s.insert(str);
+  }
+  for(auto x:s){
+      cout<<x<<endl;
+  }
+
+    return 0;
+}
