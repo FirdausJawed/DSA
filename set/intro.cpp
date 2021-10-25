9e0f4c4f78ca4a78424461f81e333612c2a113ec
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-25 18:53:49 +0530

    .

---
+++ set/intro.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main (){
+   
+   int arr[]={1,22,3,3,10,11,9,8,6,2,22};
+   int n = sizeof(arr)/sizeof(int);
+   set<int>s;
+
+   for (int i = 0; i < n; i++)
+   {
+       s.insert(arr[i]);
+   }
+   
+   //printing the set elements
+
+   for (set<int> :: iterator i = s.begin(); i != s.end(); i++)
+   {
+      cout<<*i<<" ";
+   }
+   
+    return 0;
+}
