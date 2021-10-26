a68057fdc6f85b49362207aed72f33e655c7a1f8
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2021-10-26 10:03:28 +0530

    added basics of map function

--- map/basics.cpp
+++ map/basics.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void print (map<int,string>&m){
+    cout<<m.size()<<endl;
+    for(auto pr:m){
+        cout<<pr.first<<" "<<pr.second<<endl;
+    }
+}
+int main(){
+
+ map<int,string>m;
+ m[1]="fj";
+ m[5]="fird";
+ m[3]="muskan";
+ m[2]="tgb";
+ m[7]="firdaus";
+
+m.insert({4,"the great fj"});
+print(m);
+    return 0;
+}
