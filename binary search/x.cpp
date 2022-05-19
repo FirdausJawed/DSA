c9edc81b7616f67d020c4fd3564862976304cb0e
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2022-05-19 12:25:51 +0530

    added another codechef question

--- binary search/x.cpp
+++ binary search/x.cpp
@@ -1,219 +1,88 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
-#pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h>
-#include <complex>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <list>
-#include <chrono>
-#include <random>
+// Quick sort in C++
+
 #include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <stack>
-#include <iomanip>
-#include <fstream>
-
 using namespace std;
-typedef unsigned long long ull;
-typedef long long ll;
-typedef long double ld;
-typedef pair<int, int> p32;
-typedef pair<ll, ll> p64;
-typedef pair<double, double> pdd;
-typedef vector<ll> v64;
-typedef vector<int> v32;
-typedef vector<vector<int>> vv32;
-typedef vector<vector<ll>> vv64;
-typedef vector<vector<p64>> vvp64;
-typedef vector<p64> vp64;
-typedef vector<p32> vp32;
-ll MOD = 1000000007;
-double eps = 1e-12;
-#define forn(i, n) for (ll i = 0; i < n; i++)
-#define forsn(i, s, e) for (ll i = s; i < e; i++)
-#define rforn(i, s) for (ll i = s; i >= 0; i--)
-#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
-#define ln "\n"
-#define dbg(x) cout << #x << " = " << x << ln
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-#define INF 2e18
-#define fast_cin()                    \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define all(x) (x).begin(), (x).end()
-#define al(arr, n) arr, arr + n
-#define sz(x) ((ll)(x).size())
 
-// function for prime factorization
-vector<pair<ll, ll>> pf(ll n)
+// function to swap elements
+void swap(int *a, int *b)
 {
-    vector<pair<ll, ll>> prime;
-    for (int i = 2; i <= sqrt(n); i++)
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// function to print the array
+void printArray(int array[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << array[i] << " ";
+    cout << endl;
+}
+
+// function to rearrange array (find the partition point)
+int partition(int array[], int low, int high)
+{
+
+    // select the rightmost element as pivot
+    int pivot = array[high];
+
+    // pointer for greater element
+    int i = (low - 1);
+
+    // traverse each element of the array
+    // compare them with the pivot
+    for (int j = low; j < high; j++)
     {
-        if (n % i == 0)
+        if (array[j] <= pivot)
         {
-            int count = 0;
-            while (n % i == 0)
-            {
-                count++;
-                n = n / i;
-            }
-            prime.pb(mp(i, count));
+
+            // if element smaller than pivot is found
+            // swap it with the greater element pointed by i
+            i++;
+
+            // swap element at i with element at j
+            swap(&array[i], &array[j]);
         }
     }
-    if (n > 1)
-    {
-        prime.pb(mp(n, 1));
-    }
-    return prime;
+
+    // swap pivot with the greater element at i
+    swap(&array[i + 1], &array[high]);
+
+    // return the partition point
+    return (i + 1);
 }
 
-// sum of digits of a number
-ll sumofno(ll n)
+void quickSort(int array[], int low, int high)
 {
-    ll sum = 0;
-    while (n != 0)
+    if (low < high)
     {
-        sum += n % 10;
-        n = n / 10;
-    }
-    return sum;
-}
 
-// modular exponentiation
-long long modpow(long long val, long long deg, long long mod)
-{
-    if (!deg)
-        return 1 % mod;
-    if (deg & 1)
-        return modpow(val, deg - 1, mod) * val % mod;
-    long long res = modpow(val, deg >> 1, mod);
-    return (res * res) % mod;
-}
+        // find the pivot element such that
+        // elements smaller than pivot are on left of pivot
+        // elements greater than pivot are on righ of pivot
+        int pi = partition(array, low, high);
 
-const int N = 1e6 + 100;
-long long fact[N];
-// initialise the factorial
-void initfact()
-{
-    fact[0] = 1;
-    for (int i = 1; i < N; i++)
-    {
-        fact[i] = (fact[i - 1] * i);
-        fact[i] %= MOD;
+        // recursive call on the left of pivot
+        quickSort(array, low, pi - 1);
+
+        // recursive call on the right of pivot
+        quickSort(array, pi + 1, high);
     }
 }
 
-// formula for c
-ll C(ll n, ll i)
-{
-    ll res = fact[n];
-    ll div = fact[n - i] * fact[i];
-    div %= MOD;
-    div = modpow(div, MOD - 2, MOD);
-    return (res * div) % MOD;
-}
-
-// function for fast expo
-ll fastexpo(ll a, ll b)
-{
-    if (b == 0)
-    {
-        return 1;
-    }
-    if (a == 0)
-    {
-        return 0;
-    }
-    ll y = fastexpo(a, b / 2);
-    if (b % 2 == 0)
-    {
-        return y * y;
-    }
-    else
-    {
-        return a * y * y;
-    }
-}
-
-ll popcount(ll n)
-{
-    ll c = 0;
-    for (; n; ++c)
-        n &= n - 1;
-    return c;
-}
-
-ll ce(ll x, ll y)
-{
-    ll res = x / y;
-    if (x % y != 0)
-    {
-        res++;
-    }
-    return res;
-}
-
-bool pow2(ll x)
-{
-    ll res = x & (x - 1);
-    if (res == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-void solve()
-{
-    ll n;
-    cin >> n;
-    ll s38=0, l108=0;
-    forn(i, n)
-    {
-        string s;
-        cin >> s;
-        if (s == "START38")
-        {
-            s38++;
-        }
-        else
-        {
-            l108++;
-        }
-    }
-    cout << s38 << " " << l108 << ln;
-}
-
+// Driver code
 int main()
 {
-    fast_cin();
-    ll t;
-    cin >> t;
-    for (int it = 1; it <= t; it++)
-    {
-        solve();
-    }
-    return 0;
-}
+    int data[] = {8, 7, 6, 1, 0, 9, 2};
+    int n = sizeof(data) / sizeof(data[0]);
 
-/*
-1. Check borderline constraints. Can a variable you are dividing by be 0?
-2. Use ll while using bitshifts
-3. Do not erase from set while iterating it
-4. Initialise everything
-5. Read the task carefully, is something unique, sorted, adjacent, guaranteed??
-6. DO NOT use if(!mp[x]) if you want to iterate the map later
-7. Are you using i in all loops? Are the i's conflicting?
-*/
+    cout << "Unsorted Array: \n";
+    printArray(data, n);
+
+    // perform quicksort on data
+    quickSort(data, 0, n - 1);
+
+    cout << "Sorted array in ascending order: \n";
+    printArray(data, n);
+}
